a661609d4e8235b8c900eae04bef96c4400f8a38
DataM0del <183248792+DataM0del@users.noreply.github.com>
2024-12-19 07:55:59 -0600

    add: Render2DEvent
    and also a mixin to post this Render2DEvent

---
+++ src/main/java/info/opensigma/mixin/GameRendererMixin.java
@@ -0,0 +1,25 @@
+package info.opensigma.mixin;
+
+import info.opensigma.OpenSigma;
+import info.opensigma.event.Render2DEvent;
+import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+/**
+ * Hooks [{@link GameRenderer#renderWorld(float, long, MatrixStack)}] to post [{@link Render2DEvent}] events.
+ * We aren't hooking [{@link GameRenderer#render(float, long, boolean)}
+ * because something else could call this, & we wouldn't send the event when we should've,
+ * even though it doesn't really matter.
+ */
+
+@Mixin(GameRenderer.class)
+public class GameRendererMixin {
+    @Inject(method = "renderWorld", at = @At(value = "HEAD"))
+    public void onRenderWorld(float tickDelta, long limitTime, MatrixStack matrix, CallbackInfo ci) {
+        OpenSigma.getInstance().eventBus.post(new Render2DEvent(tickDelta, limitTime));
+    }
+}
