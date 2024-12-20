d2e31ab85f39e0d5cbbc01f80f902e231948078d
DataM0del <183248792+DataM0del@users.noreply.github.com>
2024-12-20 16:06:46 -0600

    fix(INameable): add `name` field and deprecate `getName`

--- src/main/kotlin/info/opensigma/system/INameable.kt
+++ src/main/kotlin/info/opensigma/system/INameable.kt
@@ -1,5 +1,7 @@
 package info.opensigma.system
 
 interface INameable {
+    val name: String
+    @Deprecated("Use the name field instead", replaceWith = ReplaceWith("name"))
     fun getName(): String
 }
