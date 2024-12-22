6daa662d447f62594f9d345cfe73a3a9732ff0de
DataModel <183248792+DataM0del@users.noreply.github.com>
2024-12-21 22:19:15 -0500

    refactor: use requireNonNull from Kotlin std instead of NullUtils

--- src/main/kotlin/info/opensigma/setting/owner/SettingOwner.kt
+++ src/main/kotlin/info/opensigma/setting/owner/SettingOwner.kt
@@ -3,49 +3,48 @@ package info.opensigma.setting.owner
 import info.opensigma.setting.Setting
 import info.opensigma.system.ElementRepository
 import info.opensigma.system.INameable
-import net.jezevcik.argon.utils.objects.NullUtils
 
 @Suppress("UNCHECKED_CAST")
 class SettingOwner<T>(owner: INameable) : ElementRepository<Setting<T>>("${owner.name}-settings",
     Setting::class.java as Class<Setting<T>>, toScan = arrayOf(owner)) {
 
     fun getBooleanValue(name: String): Boolean {
-        return NullUtils.requireNotNull(getByName(name)?.value, "Setting $name does not exist!") as Boolean
+        return requireNotNull(getByName(name)?.value, { "Setting $name does not exist!" }) as Boolean
     }
 
     fun getIntValue(name: String): Int {
-        return NullUtils.requireNotNull(getByName(name)?.value, "Setting $name does not exist!") as Int
+        return requireNotNull(getByName(name)?.value, { "Setting $name does not exist!" }) as Int
     }
 
     fun getFloatValue(name: String): Float {
-        return NullUtils.requireNotNull(getByName(name)?.value, "Setting $name does not exist!") as Float
+        return requireNotNull(getByName(name)?.value, { "Setting $name does not exist!" }) as Float
     }
 
     fun getDoubleValue(name: String): Double {
-        return NullUtils.requireNotNull(getByName(name)?.value, "Setting $name does not exist!") as Double
+        return requireNotNull(getByName(name)?.value, { "Setting $name does not exist!" }) as Double
     }
 
     fun getLongValue(name: String): Long {
-        return NullUtils.requireNotNull(getByName(name)?.value, "Setting $name does not exist!") as Long
+        return requireNotNull(getByName(name)?.value, { "Setting $name does not exist!" }) as Long
     }
 
     fun getShortValue(name: String): Short {
-        return NullUtils.requireNotNull(getByName(name)?.value, "Setting $name does not exist!") as Short
+        return requireNotNull(getByName(name)?.value, { "Setting $name does not exist!" }) as Short
     }
 
     fun getByteValue(name: String): Byte {
-        return NullUtils.requireNotNull(getByName(name)?.value, "Setting $name does not exist!") as Byte
+        return requireNotNull(getByName(name)?.value, { "Setting $name does not exist!" }) as Byte
     }
 
     fun getCharValue(name: String): Char {
-        return NullUtils.requireNotNull(getByName(name)?.value, "Setting $name does not exist!") as Char
+        return requireNotNull(getByName(name)?.value, { "Setting $name does not exist!" }) as Char
     }
 
     fun getStringValue(name: String): String {
-        return NullUtils.requireNotNull(getByName(name)?.value, "Setting $name does not exist!") as String
+        return requireNotNull(getByName(name)?.value, { "Setting $name does not exist!" }) as String
     }
 
     fun getObjectValue(name: String): Any {
-        return NullUtils.requireNotNull(getByName(name)?.value as Any, "Setting $name does not exist!")
+        return requireNotNull(getByName(name)?.value as Any, { "Setting $name does not exist!" })
     }
 }
