7edf66361d549535b28194146d836ed995f6fcfa
DataM0del <183248792+DataM0del@users.noreply.github.com>
2024-12-22 15:45:46 -0600

    fix: IntelliJ IDEA back at it again breaking stuff by saying it's unused

--- src/main/kotlin/info/opensigma/bind/BindManager.kt
+++ src/main/kotlin/info/opensigma/bind/BindManager.kt
@@ -1,6 +1,8 @@
 package info.opensigma.bind
 
 import info.opensigma.OpenSigma
+import info.opensigma.event.impl.KeyPressEvent
+import meteordevelopment.orbit.EventHandler
 import java.util.HashMap
 import java.util.function.Supplier
 
@@ -15,5 +17,19 @@ class BindManager {
         map[integer] = runnable
     }
 
-    companion object
+    @Suppress("unused")
+    @EventHandler
+    private fun onKey(keyPressEvent: KeyPressEvent) {
+        if (keyPressEvent.keyAction != 0)
+            return
+        map.forEach { (key, value) ->
+            if (key.get() == keyPressEvent.key)
+                value.run()
+        }
+    }
+
+    companion object {
+        @Suppress("unused")
+        val instance = BindManager()
+    }
 }
