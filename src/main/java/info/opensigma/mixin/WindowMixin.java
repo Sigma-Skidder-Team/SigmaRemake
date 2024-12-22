3eaf9741d0eba66524dbbfb00afcf4b41895dab0
DataM0del <183248792+DataM0del@users.noreply.github.com>
2024-12-22 17:28:08 -0600

    chore(WindowMixin.java): modify the window title when we create the window initially

--- src/main/java/info/opensigma/mixin/WindowMixin.java
+++ src/main/java/info/opensigma/mixin/WindowMixin.java
@@ -4,6 +4,8 @@ import net.minecraft.client.util.Window;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 @Mixin(Window.class)
 public class WindowMixin {
@@ -15,5 +17,9 @@ public class WindowMixin {
     private CharSequence setTitle(CharSequence title) {
         title = ((String)title).replace("Minecraft", "OpenSigma for Minecraft");
         return title;
+    }
+    @ModifyArgs(method = "<init>", at = @At(value = "INVOKE", target = "Lorg/lwjgl/glfw/GLFW;glfwCreateWindow(IILjava/lang/CharSequence;JJ)J"))
+    private void modifyWindowTitle(Args args) {
+        args.set(2, ((String)args.get(2)).replace("Minecraft", "OpenSigma"));
     }
 }
