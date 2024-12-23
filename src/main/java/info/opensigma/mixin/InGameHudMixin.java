bee596d5bae77c1c26385ded427ecad914856689
DataM0del <183248792+DataM0del@users.noreply.github.com>
2024-12-23 08:28:14 -0600

    refactor: make Render2DEvent give `DrawContext` instead of just a `MatrixStack`

--- src/main/java/info/opensigma/mixin/InGameHudMixin.java
+++ src/main/java/info/opensigma/mixin/InGameHudMixin.java
@@ -5,7 +5,6 @@ import info.opensigma.event.impl.render.Render2DEvent;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.render.RenderTickCounter;
-import net.minecraft.client.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -15,6 +14,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class InGameHudMixin {
     @Inject(method = "render", at = @At("TAIL"))
     private void onRender(DrawContext context, RenderTickCounter tickCounter, CallbackInfo ci) {
-        OpenSigma.getInstance().getEventBus().post(new Render2DEvent(context.getMatrices(), tickCounter.getTickDelta(true)));
+        OpenSigma.getInstance().getEventBus().post(new Render2DEvent(tickCounter.getTickDelta(true), context));
     }
 }
--- src/main/kotlin/info/opensigma/event/impl/render/Render2DEvent.kt
+++ src/main/kotlin/info/opensigma/event/impl/render/Render2DEvent.kt
@@ -1,8 +1,8 @@
 package info.opensigma.event.impl.render
 
-import net.minecraft.client.util.math.MatrixStack
+import net.minecraft.client.gui.DrawContext
 
 data class Render2DEvent(
-    val matrices: MatrixStack,
-    val tickDelta: Float = 0.0f
+    val tickDelta: Float = 0.0f,
+    val context: DrawContext
 )
