67791e836391863ffdef1a515c861a24f3619cc2
noarc <86848614+MarkGG8181@users.noreply.github.com>
2024-12-23 18:35:39 +0200

    loadShader accessor

--- src/main/java/com/skidders/sigma/mixin/GameRendererMixin.java
+++ src/main/java/com/skidders/sigma/mixin/GameRendererMixin.java
@@ -2,19 +2,32 @@ package com.skidders.sigma.mixin;
 
 import com.skidders.SigmaReborn;
 import com.skidders.sigma.events.impl.Render3DEvent;
+import com.skidders.sigma.screens.GameRendererAccessor;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GameRenderer.class)
-public class GameRendererMixin {
+public abstract class GameRendererMixin implements GameRendererAccessor {
 
     @Inject(method = "renderWorld", at = @At(value = "HEAD"))
     public void onRenderWorld(float tickDelta, long limitTime, MatrixStack matrix, CallbackInfo ci) {
         SigmaReborn.EVENT_BUS.post(new Render3DEvent(matrix, tickDelta, limitTime));
+    }
+
+    @Shadow
+    private void loadShader(Identifier identifier) {
+
+    }
+
+    @Override
+    public void invokeLoadShader(Identifier identifier) {
+        loadShader(identifier);
     }
 
 }
---
+++ src/main/java/com/skidders/sigma/screens/GameRendererAccessor.java
@@ -0,0 +1,7 @@
+package com.skidders.sigma.screens;
+
+import net.minecraft.util.Identifier;
+
+public interface GameRendererAccessor {
+    void invokeLoadShader(Identifier identifier);
+}
