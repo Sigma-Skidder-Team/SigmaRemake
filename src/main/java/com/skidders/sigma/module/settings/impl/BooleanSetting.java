4b25749db6b348ea03017f631185bba3b9a389e7
noarc <86848614+MarkGG8181@users.noreply.github.com>
2024-12-25 20:40:56 +0200

    Update BooleanSetting.java

--- src/main/java/com/skidders/sigma/module/settings/impl/BooleanSetting.java
+++ src/main/java/com/skidders/sigma/module/settings/impl/BooleanSetting.java
@@ -1,9 +1,12 @@
 package com.skidders.sigma.module.settings.impl;
 
 import com.skidders.sigma.module.settings.Setting;
+import com.skidders.sigma.screens.components.CheckboxComponent;
 
 public class BooleanSetting extends Setting<Boolean> {
+    public CheckboxComponent checkboxComponent;
     public BooleanSetting(String name, String desc, boolean value) {
         super(name, desc, value);
+        this.checkboxComponent = new CheckboxComponent();
     }
 }
