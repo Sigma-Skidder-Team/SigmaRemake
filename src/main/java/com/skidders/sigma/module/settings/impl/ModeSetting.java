aaaff42921597e779775e3de38e4b207a5cfcd04
noarc <86848614+MarkGG8181@users.noreply.github.com>
2024-12-25 20:40:46 +0200

    string setting -> mode setting

--- src/main/java/com/skidders/sigma/module/settings/impl/ModeSetting.java
+++ src/main/java/com/skidders/sigma/module/settings/impl/ModeSetting.java
@@ -2,11 +2,11 @@ package com.skidders.sigma.module.settings.impl;
 
 import com.skidders.sigma.module.settings.Setting;
 
-public class StringSetting extends Setting<String> {
+public class ModeSetting extends Setting<String> {
 
     public String[] allValues;
 
-    public StringSetting(String name, String desc, String value, String[] allValues) {
+    public ModeSetting(String name, String desc, String value, String[] allValues) {
         super(name, desc, value);
         this.allValues = allValues;
     }
