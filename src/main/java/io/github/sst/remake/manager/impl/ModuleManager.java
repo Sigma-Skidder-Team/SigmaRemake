bfd0e6c317f88be920b4dc84b9660fb0bbe4f45c
almondeee <john.ash.wood13@gmail.com>
2025-12-18 20:00:27 +0200

    BrainFreeze module

--- src/main/java/io/github/sst/remake/manager/impl/ModuleManager.java
+++ src/main/java/io/github/sst/remake/manager/impl/ModuleManager.java
@@ -1,11 +1,14 @@
 package io.github.sst.remake.manager.impl;
 
 import io.github.sst.remake.manager.Manager;
+import io.github.sst.remake.module.Category;
 import io.github.sst.remake.module.Module;
+import io.github.sst.remake.module.impl.BrainFreezeModule;
 import io.github.sst.remake.module.impl.TestModule;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ModuleManager extends Manager {
 
@@ -16,6 +19,7 @@ public class ModuleManager extends Manager {
     @Override
     public void init() {
         modules.add(new TestModule());
+        modules.add(new BrainFreezeModule());
         initModules();
         super.init();
     }
@@ -28,5 +32,15 @@ public class ModuleManager extends Manager {
 
     private void initModules() {
         modules.forEach(Module::onInit);
+    }
+
+    public Module getModule(String input) {
+        return this.modules.stream().filter(m -> m.name.equalsIgnoreCase(input)).findFirst().orElse(null);
+    }
+
+    public List<Module> getModulesByCategory(Category input) {
+        return this.modules.stream().filter(mod ->
+                mod.category.equals(input)).collect(Collectors.toList()
+        );
     }
 }
