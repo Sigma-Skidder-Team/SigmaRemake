03f9878dd22c534793b5b2cbb2c7af70f282af48
almondeee <john.ash.wood13@gmail.com>
2025-12-31 22:29:59 +0200

    Add ClientPlayerTickEvent and Mixin for player tick injection

---
+++ src/main/java/io/github/sst/remake/mixin/MixinClientPlayerEntity.java
@@ -0,0 +1,18 @@
+package io.github.sst.remake.mixin;
+
+import io.github.sst.remake.event.impl.player.ClientPlayerTickEvent;
+import net.minecraft.client.network.ClientPlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ClientPlayerEntity.class)
+public class MixinClientPlayerEntity {
+
+    @Inject(method = "tick", at = @At("HEAD"))
+    private void injectTick(CallbackInfo ci) {
+        new ClientPlayerTickEvent().call();
+    }
+
+}
