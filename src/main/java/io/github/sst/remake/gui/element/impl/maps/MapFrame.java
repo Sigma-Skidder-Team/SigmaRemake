a41df0f59cc0c11f3a4c8122beae96b19a657c3c
almondeee <john.ash.wood13@gmail.com>
2026-01-01 19:30:40 +0200

    Fix null pointer exception in MapFrame rendering logic

--- src/main/java/io/github/sst/remake/gui/element/impl/maps/MapFrame.java
+++ src/main/java/io/github/sst/remake/gui/element/impl/maps/MapFrame.java
@@ -123,55 +123,57 @@ public class MapFrame extends Element implements IMinecraft {
             this.field20647.field20687 = true;
         }
 
-        int var6 = Math.max(this.width, this.height);
-        int var7 = (this.width - var6) / 2;
-        int var8 = (this.height - var6) / 2;
-        float var9 = (float) this.field20649 / ((float) this.field20649 - 1.0F);
-        float var10 = (float) var6 / ((float) this.field20649 * 2.0F);
-        double var11 = ((double) this.field20650 - Math.floor(this.field20650)) * (double) var10;
-        double var13 = ((double) this.field20651 - Math.floor(this.field20651)) * (double) var10;
-        TextureManager textureManager = client.getTextureManager();
-        textureManager.bindTexture(TextureManager.MISSING_IDENTIFIER);
-        ScissorUtils.startScissor(this.x, this.y, this.x + this.width, this.y + this.height, true);
-        GL11.glPushMatrix();
-        GL11.glTranslatef((float) (this.x + this.width / 2), (float) (this.y + this.height / 2), 0.0F);
-        GL11.glScalef(var9, var9, 0.0F);
-        GL11.glRotatef(-90.0F, 0.0F, 0.0F, 1.0F);
-        GL11.glTranslatef((float) (-this.x - this.width / 2), (float) (-this.y - this.height / 2), 0.0F);
-        GL11.glTranslated(-var11, var13, 0.0);
-        RenderUtils.drawTexturedQuad(
-                (float) (this.x + var7),
-                (float) (this.y + var8),
-                (float) var6,
-                (float) var6,
-                this.field20654.field30546,
-                ClientColors.LIGHT_GREYISH_BLUE.getColor(),
-                0.0F,
-                0.0F,
-                (float) this.field20654.field30544,
-                (float) this.field20654.field30545,
-                true,
-                false
-        );
-        GL11.glPopMatrix();
-
-        for (Waypoint var16 : Client.INSTANCE.waypointManager.getWaypoints()) {
-            float var17 = (float) (this.chunkPos.x * 16) - this.field20651 * 16.0F;
-            float var18 = (float) (this.chunkPos.z * 16) - this.field20650 * 16.0F;
-            float var19 = (float) var16.x - var17 + 1.0F;
-            float var20 = (float) var16.z - var18 + 1.0F;
-            float var21 = (float) var6 / ((float) (this.field20649 - 1) * 2.0F);
-            RenderUtils.drawImage(
-                    (float) (this.x + Math.round(var19 * var21 / 16.0F) + this.width / 2 - 16),
-                    (float) (this.y + Math.round(var20 * var21 / 16.0F) + this.height / 2 - 42),
-                    32.0F,
-                    46.0F,
-                    Resources.waypointPNG,
-                    var16.color
+        if (this.field20654 != null) {
+            int var6 = Math.max(this.width, this.height);
+            int var7 = (this.width - var6) / 2;
+            int var8 = (this.height - var6) / 2;
+            float var9 = (float) this.field20649 / ((float) this.field20649 - 1.0F);
+            float var10 = (float) var6 / ((float) this.field20649 * 2.0F);
+            double var11 = ((double) this.field20650 - Math.floor(this.field20650)) * (double) var10;
+            double var13 = ((double) this.field20651 - Math.floor(this.field20651)) * (double) var10;
+            TextureManager textureManager = client.getTextureManager();
+            textureManager.bindTexture(TextureManager.MISSING_IDENTIFIER);
+            ScissorUtils.startScissor(this.x, this.y, this.x + this.width, this.y + this.height, true);
+            GL11.glPushMatrix();
+            GL11.glTranslatef((float) (this.x + this.width / 2), (float) (this.y + this.height / 2), 0.0F);
+            GL11.glScalef(var9, var9, 0.0F);
+            GL11.glRotatef(-90.0F, 0.0F, 0.0F, 1.0F);
+            GL11.glTranslatef((float) (-this.x - this.width / 2), (float) (-this.y - this.height / 2), 0.0F);
+            GL11.glTranslated(-var11, var13, 0.0);
+            RenderUtils.drawTexturedQuad(
+                    (float) (this.x + var7),
+                    (float) (this.y + var8),
+                    (float) var6,
+                    (float) var6,
+                    this.field20654.field30546,
+                    ClientColors.LIGHT_GREYISH_BLUE.getColor(),
+                    0.0F,
+                    0.0F,
+                    (float) this.field20654.field30544,
+                    (float) this.field20654.field30545,
+                    true,
+                    false
             );
-        }
+            GL11.glPopMatrix();
 
-        ScissorUtils.restoreScissor();
+            for (Waypoint var16 : Client.INSTANCE.waypointManager.getWaypoints()) {
+                float var17 = (float) (this.chunkPos.x * 16) - this.field20651 * 16.0F;
+                float var18 = (float) (this.chunkPos.z * 16) - this.field20650 * 16.0F;
+                float var19 = (float) var16.x - var17 + 1.0F;
+                float var20 = (float) var16.z - var18 + 1.0F;
+                float var21 = (float) var6 / ((float) (this.field20649 - 1) * 2.0F);
+                RenderUtils.drawImage(
+                        (float) (this.x + Math.round(var19 * var21 / 16.0F) + this.width / 2 - 16),
+                        (float) (this.y + Math.round(var20 * var21 / 16.0F) + this.height / 2 - 42),
+                        32.0F,
+                        46.0F,
+                        Resources.waypointPNG,
+                        var16.color
+                );
+            }
+
+            ScissorUtils.restoreScissor();
+        }
         int var22 = Math.round((float) (this.chunkPos.x * 16) - this.field20651 * 16.0F);
         int var23 = Math.round((float) (this.chunkPos.z * 16) - this.field20650 * 16.0F);
         String var24 = var22 + "  " + var23;
