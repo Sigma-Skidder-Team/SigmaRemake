6d6933a0bf43687787a09931a3f65dddd2829ccb
almondeee <john.ash.wood13@gmail.com>
2026-01-05 20:34:42 +0200

    Basic audio streaming

---
+++ src/main/java/io/github/sst/remake/util/io/audio/stream/BasicAudioProcessor.java
@@ -0,0 +1,7 @@
+package io.github.sst.remake.util.io.audio.stream;
+
+public class BasicAudioProcessor implements AudioProcessor {
+    @Override
+    public void processBuffer(byte[] var1, int var2, int var3) {
+    }
+}
---
+++ src/main/java/io/github/sst/remake/util/io/audio/stream/MusicByteStream.java
@@ -0,0 +1,15 @@
+package io.github.sst.remake.util.io.audio.stream;
+
+import java.io.ByteArrayOutputStream;
+
+public final class MusicByteStream extends ByteArrayOutputStream {
+    public final MusicStream musicStream;
+
+    public MusicByteStream(MusicStream musicStream) {
+        this.musicStream = musicStream;
+    }
+
+    public byte[] getBuffer() {
+        return this.buf;
+    }
+}
