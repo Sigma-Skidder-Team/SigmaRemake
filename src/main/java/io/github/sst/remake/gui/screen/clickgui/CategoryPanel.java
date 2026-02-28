fb151ba31fc079aff66b259e5582bcaac5d932f0
rmpb <mark@wonderland.ac>
2026-02-28 14:48:40 +0200

    refactor: category panel

--- src/main/java/io/github/sst/remake/gui/screen/clickgui/CategoryPanel.java
+++ src/main/java/io/github/sst/remake/gui/screen/clickgui/CategoryPanel.java
@@ -19,17 +19,21 @@ public class CategoryPanel extends Widget {
     public final Category category;
     public ModuleList moduleList;
     public float expandProgress;
-    public int baseX;
-    public int baseY;
-    private int panelCornerRadius;
+    public int expandedBaseX;
+    public int expandedBaseY;
+    private int cornerRadius;
     private final List<ModuleClickListener> moduleClickListeners = new ArrayList<>();
 
-    public CategoryPanel(GuiComponent var1, int var3, int var4, Category category) {
-        super(var1, category.toString(), var3, var4, 200, 350, true);
+    public CategoryPanel(GuiComponent parent, int x, int y, Category category) {
+        super(parent, category.toString(), x, y, 200, 350, true);
+
         this.setWidth(200);
         this.setHeight(350);
+
         this.enableImmediateDrag = true;
+
         this.category = category;
+
         this.initModuleList();
     }
 
@@ -41,51 +45,50 @@ public class CategoryPanel extends Widget {
     }
 
     private void initModuleList() {
-        this.addToList(this.moduleList = new ModuleList(this, "modListView", 0, 60, this.getWidth(), this.getHeight() - 60, this.category));
+        this.moduleList = new ModuleList(this, "modListView", 0, 60, this.getWidth(), this.getHeight() - 60, this.category);
+        this.addToList(this.moduleList);
+
         this.moduleList.addWidthSetter(new ModuleListResizer());
-        this.moduleList.addWidthSetter((var0, var1) -> {
-            var0.setY(60);
-            var0.setHeight(var1.getHeight() - 60);
+        this.moduleList.addWidthSetter((list, panel) -> {
+            list.setY(60);
+            list.setHeight(panel.getHeight() - 60);
         });
     }
 
     @Override
     public void updatePanelDimensions(int mouseX, int mouseY) {
-        if (!(this.expandProgress >= 1.0F)) {
+        if (this.expandProgress < 1.0F) {
             this.setDraggable(false);
             this.isMouseDownOverComponent = false;
         } else {
-            this.baseX = this.getX();
-            this.baseY = this.getY();
+            this.expandedBaseX = this.getX();
+            this.expandedBaseY = this.getY();
             this.setDraggable(true);
         }
 
-        float var5 = 200.0F;
-        float var6 = 320.0F;
-        float var7 = 0.7F;
-        float var8 = 0.1F;
-        int var9 = (int) (200.0F + 140.0F * (1.0F - this.expandProgress));
-        int var10 = (int) (320.0F + 320.0F * 0.1F * (1.0F - this.expandProgress));
-        int var11 = this.baseY;
-        int var12 = (int) ((float) this.baseX - ((float) var9 - 200.0F) / 2.0F + 0.5F);
+        int targetWidth = (int) (200.0F + 140.0F * (1.0F - this.expandProgress));
+        int targetHeight = (int) (320.0F + 320.0F * 0.1F * (1.0F - this.expandProgress));
+
+        int newY = this.expandedBaseY;
+        int newX = (int) ((float) this.expandedBaseX - ((float) targetWidth - 200.0F) / 2.0F + 0.5F);
+
         if (this.expandProgress < 1.0F) {
-            if (var12 < 0) {
-                var12 = 0;
+            if (newX < 0) {
+                newX = 0;
             }
-
-            if (var12 + var9 > this.parent.getWidth()) {
-                var12 = this.parent.getWidth() - var9;
+            if (newX + targetWidth > this.parent.getWidth()) {
+                newX = this.parent.getWidth() - targetWidth;
             }
-
-            if (var11 + var10 > this.parent.getHeight()) {
-                var11 = this.parent.getHeight() - var10;
+            if (newY + targetHeight > this.parent.getHeight()) {
+                newY = this.parent.getHeight() - targetHeight;
             }
         }
 
-        this.setWidth(var9);
-        this.setHeight(var10);
-        this.setX(var12);
-        this.setY(var11);
+        this.setWidth(targetWidth);
+        this.setHeight(targetHeight);
+        this.setX(newX);
+        this.setY(newY);
+
         super.updatePanelDimensions(mouseX, mouseY);
     }
 
@@ -93,22 +96,29 @@ public class CategoryPanel extends Widget {
     public void draw(float partialTicks) {
         super.applyScaleTransforms();
         super.applyTranslationTransforms();
-        int var4 = (int) (1.0F + 10.0F * (1.0F - this.expandProgress));
+
+        int inset = (int) (1.0F + 10.0F * (1.0F - this.expandProgress));
+
         RenderUtils.drawRoundedRect(
-                (float) (this.getX() + (var4 - 1)),
-                (float) (this.getY() + (var4 - 1)),
-                (float) (this.getWidth() - (var4 - 1) * 2),
-                (float) (this.getHeight() - (var4 - 1) * 2),
-                (float) this.panelCornerRadius + (1.0F - this.expandProgress) * (float) var4,
+                (float) (this.getX() + (inset - 1)),
+                (float) (this.getY() + (inset - 1)),
+                (float) (this.getWidth() - (inset - 1) * 2),
+                (float) (this.getHeight() - (inset - 1) * 2),
+                (float) this.cornerRadius + (1.0F - this.expandProgress) * (float) inset,
                 partialTicks
         );
+
         RenderUtils.drawRoundedRect(
                 (float) this.getX(),
                 (float) this.getY(),
                 (float) (this.getX() + this.getWidth()),
                 (float) (this.getY() + 60),
-                ColorHelper.applyAlpha(ClientColors.LIGHT_GREYISH_BLUE.getColor(), Math.min(1.0F, partialTicks * 0.9F * this.expandProgress))
+                ColorHelper.applyAlpha(
+                        ClientColors.LIGHT_GREYISH_BLUE.getColor(),
+                        Math.min(1.0F, partialTicks * 0.9F * this.expandProgress)
+                )
         );
+
         RenderUtils.drawRoundedRect2(
                 (float) this.getX(),
                 (float) this.getY() + 60.0F * this.expandProgress,
@@ -116,13 +126,8 @@ public class CategoryPanel extends Widget {
                 (float) this.getHeight() - 60.0F * this.expandProgress,
                 ColorHelper.applyAlpha(ClientColors.LIGHT_GREYISH_BLUE.getColor(), partialTicks)
         );
-        if (!(this.expandProgress > 0.8F)) {
-            if (this.expandProgress < 0.2F) {
-                this.panelCornerRadius = 30;
-            }
-        } else {
-            this.panelCornerRadius = 20;
-        }
+
+        updateCornerRadius();
 
         String categoryName = this.getCategory().toString();
         RenderUtils.drawString(
@@ -134,9 +139,11 @@ public class CategoryPanel extends Widget {
                 FontAlignment.LEFT,
                 FontAlignment.CENTER
         );
+
         GL11.glPushMatrix();
         super.draw(partialTicks * partialTicks);
         GL11.glPopMatrix();
+
         if (this.moduleList.getScrollOffset() > 0) {
             RenderUtils.drawImage(
                     (float) this.getX(),
@@ -146,6 +153,16 @@ public class CategoryPanel extends Widget {
                     Resources.SHADOW_BOTTOM,
                     ColorHelper.applyAlpha(ClientColors.LIGHT_GREYISH_BLUE.getColor(), partialTicks * this.expandProgress * 0.5F)
             );
+        }
+    }
+
+    private void updateCornerRadius() {
+        if (this.expandProgress > 0.8F) {
+            this.cornerRadius = 20;
+            return;
+        }
+        if (this.expandProgress < 0.2F) {
+            this.cornerRadius = 30;
         }
     }
 
@@ -164,6 +181,6 @@ public class CategoryPanel extends Widget {
     }
 
     public interface ModuleClickListener {
-       void onModuleSelected(Module module);
+        void onModuleSelected(Module module);
     }
 }
--- src/main/java/io/github/sst/remake/module/Category.java
+++ src/main/java/io/github/sst/remake/module/Category.java
@@ -1,18 +1,24 @@
 package io.github.sst.remake.module;
 
 public enum Category {
-    RENDER,
-    PLAYER,
-    COMBAT,
-    WORLD,
-    MISC,
-    EXPLOIT,
-    MOVEMENT,
-    GUI,
-    ITEM;
+    RENDER("Render"),
+    PLAYER("Player"),
+    COMBAT("Combat"),
+    WORLD("World"),
+    MISC("Misc"),
+    EXPLOIT("Exploit"),
+    MOVEMENT("Movement"),
+    GUI("Gui"),
+    ITEM("Item");
+
+    public final String name;
+
+    Category(String name) {
+        this.name = name;
+    }
 
     @Override
     public String toString() {
-        return this.name().charAt(0) + this.name().substring(1).toLowerCase();
+        return name;
     }
 }
