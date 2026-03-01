ce4cf188f23c6bf17f221b3f3c76f92ebbc4f9bc
rmpb <mark@wonderland.ac>
2026-03-02 00:12:06 +0200

    chore: bump version

--- src/main/java/io/github/sst/remake/Client.java
+++ src/main/java/io/github/sst/remake/Client.java
@@ -9,7 +9,7 @@ import org.apache.logging.log4j.Logger;
 
 public class Client implements IMinecraft {
 	public static final Logger LOGGER = LogManager.getLogger("Sigma");
-    public static final String VERSION = "1.0.2";
+    public static final String VERSION = "1.0.3";
     public static final Client INSTANCE = new Client();
     public static final EventBus BUS = new EventBus();
 
